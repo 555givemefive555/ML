7334525a3ce4aa9e4912d309d57652382fc2a164
555givemefive555 <149387021+555givemefive555@users.noreply.github.com>
2026-08-18 21:20:25 +0700

    Update linear_regression.py

--- Classic models of machine learning/linear_regression.py
+++ Classic models of machine learning/linear_regression.py
@@ -1,5 +1,6 @@
 from matplotlib import pyplot as plt
 import numpy as np
+import random
 
 nums_rooms = [4, 1, 4, 2, 3, 5]
 prices = [500, 120, 450, 230, 400, 700]
@@ -26,41 +27,45 @@ plt.title('Обучение линейной регрессии')
 plt.plot(nums_rooms, prices, 'ro', label='Реальные данные')
 
 #Линейная регрессия(квадратичный подход)
-def linear_regression_square(learning_rate = 0.01, original_base_price = 50, original_price_per_room = 100):
+def linear_regression_square(nums_rooms, prices, learning_rate = 0.1, epochs = 1000):
     print(f"Квадратичный подход")
-    base_price = original_base_price
-    price_per_room = original_price_per_room
-    for i in range(len(prices)):
-        price = prices[i]
-        num_rooms = nums_rooms[i]
+    base_price = random.uniform(0, 100)
+    price_per_room = random.uniform(0, 200)
+    for epoch in range(epochs):
+        index = random.randint(0, len(nums_rooms)-1)
+        price = prices[index]
+        num_rooms = nums_rooms[index]
         price_per_room, base_price = square_trick(base_price, price_per_room, num_rooms, price, learning_rate)
-        print(f"Эпоха №{i+1}")
-        print(f"Цена за комнату: {price_per_room}")
-        print(f"Начальная цена: {base_price}")
-    
+        if epoch % 200 == 0:
+            print(f"Эпоха №{epoch+1}")
+            print(f"Цена за комнату: {price_per_room}")
+            print(f"Начальная цена: {base_price}")
+            
     x = np.linspace(0, 6, 10)
     y = base_price + price_per_room * x
     plt.plot(x, y, label='Модель с квадратичным подходом', color = "blue")
  
 #Линейная регрессия(абсолютный подход)
-def linear_regression_absolute(learning_rate = 0.01, original_base_price = 50, original_price_per_room = 100):
+def linear_regression_absolute(nums_rooms, prices, learning_rate = 0.1, epochs = 1000):
     print(f"Абсолютный подход")
-    base_price = original_base_price
-    price_per_room = original_price_per_room
-    for i in range(len(prices)):
-         price = prices[i]
-         num_rooms = nums_rooms[i]
-         price_per_room, base_price = absolute_trick(base_price, price_per_room, num_rooms, price, learning_rate)
-         print(f"Эпоха №{i+1}")
-         print(f"Цена за комнату: {price_per_room}")
-         print(f"Начальная цена: {base_price}")
-    
+    base_price = random.uniform(0, 100)
+    price_per_room = random.uniform(0, 200)
+    for epoch in range(epochs):
+        index = random.randint(0, len(nums_rooms)-1)
+        price = prices[index]
+        num_rooms = nums_rooms[index]
+        price_per_room, base_price = absolute_trick(base_price, price_per_room, num_rooms, price, learning_rate)
+        if epoch % 200 == 0:
+            print(f"Эпоха №{epoch+1}")
+            print(f"Цена за комнату: {price_per_room}")
+            print(f"Начальная цена: {base_price}")
+       
     x = np.linspace(0, 6, 10)
     y = base_price + price_per_room * x
     plt.plot(x, y, label='Модель с абсолютным подходом', color = "green")
 
-linear_regression_square()
-linear_regression_absolute()
+linear_regression_square(nums_rooms, prices)
+linear_regression_absolute(nums_rooms, prices)
 plt.legend()
 plt.grid(True)
 plt.show()
